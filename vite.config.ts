03675486a59676f285d235e280457f217d61a067
SLIMMIND <slimmind@gmail.com>
2025-02-01 12:12:04 +0300

    ability to extend existing items

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-})
+	plugins: [react(), TanStackRouterVite()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://192.168.0.185:3000',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
+});
