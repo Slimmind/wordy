501b9c721d0a943a8d7d3fa81f7cd4627b4e251a
SLIMMIND <slimmind@gmail.com>
2025-11-15 12:45:16 +0300

    improved Search by adding rules to input

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -10,67 +10,75 @@ import { useSearch } from './useSearch';
 import Button from '../button';
 
 export const Search = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const { error } = useSelector((state: RootState) => state.firestore);
-  const { searchResults, updateSearchResults } = useSearch();
+	const dispatch = useDispatch<AppDispatch>();
+	const { error } = useSelector((state: RootState) => state.firestore);
+	const { searchResults, updateSearchResults } = useSearch();
 
-  const [displayValue, setDisplayValue] = useState<string>('');
-  const deferredDisplayValue = useDeferredValue(displayValue);
+	const [displayValue, setDisplayValue] = useState<string>('');
+	const deferredDisplayValue = useDeferredValue(displayValue);
 
-  // Подписка на данные из Firebase
-  useEffect(() => {
-    const unsubscribe = dispatch(listenToItems());
-    return () => {
-      unsubscribe();
-    };
-  }, [dispatch]);
+	// Подписка на данные из Firebase
+	useEffect(() => {
+		const unsubscribe = dispatch(listenToItems());
+		return () => {
+			unsubscribe();
+		};
+	}, [dispatch]);
 
-  // Запуск поиска при изменении displayValue
-  useEffect(() => {
-    updateSearchResults(displayValue);
-  }, [displayValue, updateSearchResults]);
+	// Запуск поиска при изменении displayValue
+	useEffect(() => {
+		updateSearchResults(displayValue);
+	}, [displayValue, updateSearchResults]);
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const value = event.target.value.toLowerCase();
-    setDisplayValue(value);
-  };
+	const handleInputChange = (
+		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		// const value = event.target.value.toLowerCase();
+		const value = event.target.value
+			.replace(/[^a-zA-Z\s]/g, '')
+			.toLocaleLowerCase();
+		setDisplayValue(value);
+	};
 
-  if (error) return <div>Error: {error}</div>;
+	if (error) return <div>Error: {error}</div>;
 
-  return (
-    <InternalWindow title='Search'>
-      <Input
-        type='search'
-        id='search'
-        value={displayValue}
-        onChange={handleInputChange}
-        placeholder='Start typing the word...'
-        autoFocus
-      />
-      {searchResults.length > 0 && (
-        <ul>
-          {searchResults.map((item) => (
-            <Word key={item.id} word={item} searchQuery={deferredDisplayValue} />
-          ))}
-        </ul>
-      )}
-      {displayValue.length > 1 && searchResults.length === 0 && (
-        <>
-          <p>Ой!.. Такого слова еще нет в списке :(</p>
-          <p>
-            Хотите{' '}
-            <Link
-              to='/add-item/$searchQuery'
-              params={{ searchQuery: deferredDisplayValue }}
-            >
-              <Button>добавить</Button>
-            </Link>{' '}
-            его?
-          </p>
-        </>
-      )}
-    </InternalWindow>
-  );
+	return (
+		<InternalWindow title='Search'>
+			<Input
+				type='search'
+				id='search'
+				value={displayValue}
+				onChange={handleInputChange}
+				placeholder='Start typing the word...'
+				pattern='[a-zA-Z\s]'
+				autoFocus
+			/>
+			{searchResults.length > 0 && (
+				<ul>
+					{searchResults.map((item) => (
+						<Word
+							key={item.id}
+							word={item}
+							searchQuery={deferredDisplayValue}
+						/>
+					))}
+				</ul>
+			)}
+			{displayValue.length > 1 && searchResults.length === 0 && (
+				<>
+					<p>Ой!.. Такого слова еще нет в списке :(</p>
+					<p>
+						Хотите{' '}
+						<Link
+							to='/add-item/$searchQuery'
+							params={{ searchQuery: deferredDisplayValue }}
+						>
+							<Button>добавить</Button>
+						</Link>{' '}
+						его?
+					</p>
+				</>
+			)}
+		</InternalWindow>
+	);
 };
